290d0726e588d78491b730a3683c8532ed1a472c
Matthew Jagiello <mjagiello@gmail.com>
2020-01-23 20:54:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
   data.each do |key, value|
-    pigeons[:key] = value
+    value.each do |key1, value1}
+    pigeons[:key] = valu
   end
   puts pigeons
   return pigeons
