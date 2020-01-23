d47f796da71ff4351b3adc14cb3ee229ec3ba27f
Matthew Jagiello <mjagiello@gmail.com>
2020-01-23 21:40:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  #pigeons["Theo"] = data[:color][:purple]
+  pigeons["Theo"] = data[:color][:purple]
   puts pigeons
   return pigeons
 end
