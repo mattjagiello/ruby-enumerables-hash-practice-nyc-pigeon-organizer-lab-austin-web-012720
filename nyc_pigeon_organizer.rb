7d8db460d8098e8220b87ddfc437470e5c608be9
Matthew Jagiello <mjagiello@gmail.com>
2020-01-23 22:25:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,10 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
-  data.each do |prop, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !pigeons.has_key?(name)
-          pigeons[name] = {}
-        end
-
-        if !pigeons[name].has_key?(prop)
-          pigeons[name][prop] = []
-        end
-
-        if !pigeons[name][prop].include?(attribute)
-          pigeons[name][prop] << attribute.to_s
-        end
+  data.each do |key, value|
+    value.each do |key1, value1|
+      value1.each do |pigeon_name|
+        pigeons[pigeon_name] = {}
+        pigeons[key2][key] = []
       end
     end
   end
